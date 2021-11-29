a83e081ddcdaab0e27b13bb640262c1b8e621fb8
Nitin sagar <ns85596@gmail.com>
2021-11-29 03:41:16 -0800

    Getting Ready for Production

---
+++ build/precache-manifest.6cae0157510e15bc2e89ad3eabcc41fe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8c8204db4b7e728b0bb608a0c56bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c43dffca7289161891e",
+    "url": "/static/js/2.b94e0148.chunk.js"
+  },
+  {
+    "revision": "f3a61d2d98728a54568d625465fb46b3",
+    "url": "/static/js/2.b94e0148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634f678fcb6de15cc283",
+    "url": "/static/js/main.7c704207.chunk.js"
+  },
+  {
+    "revision": "6613802de99d406509fd",
+    "url": "/static/js/runtime-main.8d5285fa.js"
+  },
+  {
+    "revision": "77505af842b01aa2a4b789a5f159681c",
+    "url": "/static/media/CardIos.77505af8.png"
+  },
+  {
+    "revision": "a69bd473fc9eb435cf791b8beaf29e93",
+    "url": "/static/media/Discord-black.a69bd473.png"
+  },
+  {
+    "revision": "2f71ab5383293f63985ac8d5c632b3d4",
+    "url": "/static/media/Discord.2f71ab53.png"
+  },
+  {
+    "revision": "f525408caedf41bbecfbf0ddce90fb05",
+    "url": "/static/media/Illustration6.f525408c.png"
+  },
+  {
+    "revision": "89361977b103330d20d6a461591babce",
+    "url": "/static/media/Illustration7.89361977.png"
+  },
+  {
+    "revision": "5057f10d6cdd7512673999a6be54059b",
+    "url": "/static/media/background.5057f10d.png"
+  },
+  {
+    "revision": "2f71ab5383293f63985ac8d5c632b3d4",
+    "url": "/static/media/discord.2f71ab53.png"
+  },
+  {
+    "revision": "c95ddbf170a23035c5a875184f90543c",
+    "url": "/static/media/illustation.c95ddbf1.png"
+  },
+  {
+    "revision": "37b8561e072d90a415be3040f51adc49",
+    "url": "/static/media/illustration10.37b8561e.png"
+  },
+  {
+    "revision": "63f83791725c14d01328eae7d42c14f8",
+    "url": "/static/media/illustration2.63f83791.png"
+  },
+  {
+    "revision": "9cacf923dcf71421ff3887355da19ac4",
+    "url": "/static/media/illustration3.9cacf923.png"
+  },
+  {
+    "revision": "ef0c30fc206b78d1da598c404ad65e94",
+    "url": "/static/media/illustration4.ef0c30fc.png"
+  },
+  {
+    "revision": "c5356a9aea6f36d21ffea3fde6801dae",
+    "url": "/static/media/illustration5.c5356a9a.png"
+  },
+  {
+    "revision": "b79ee6e75163025b10aa729c27db86ff",
+    "url": "/static/media/illustration8.b79ee6e7.png"
+  }
+]);
