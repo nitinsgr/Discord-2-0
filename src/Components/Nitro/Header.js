7183840130f342571d69ef9aee4e11a509d32fcf
Nitin sagar <ns85596@gmail.com>
2021-11-29 04:12:19 -0800

    Added files

--- src/Components/Nitro/Header.js
+++ src/Components/Nitro/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Nitro from '../../../Images/Nitro.png'
+import Nitro from '../../Images/Nitro.png'
 
 const Container = styled.div`
 
